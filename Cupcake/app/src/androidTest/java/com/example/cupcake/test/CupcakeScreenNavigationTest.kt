c84e37648779d0f6d89e6e93a43e20efd7ca2b5c
Stephen Choo <stephen.w.choo@gmail.com>
2023-01-23 15:29:37 +1100

    Written the Android tests, having trouble with running due to current internet - will try rerunning again when connection improves

---
+++ Cupcake/app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -0,0 +1,126 @@
+package com.example.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performRotaryScrollInput
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import com.example.cupcake.CupcakeApp
+import com.example.cupcake.CupcakeScreen
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CupcakeScreenNavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupCupcakeNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+            CupcakeApp()
+        }
+    }
+
+//helpers
+
+    private fun getFormattedDate(): String {
+        // gets the date for the next day
+        val calendar = Calendar.getInstance()
+        calendar.add(java.util.Calendar.DATE, 1)
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        return formatter.format(calendar.time)
+    }
+
+    private fun navigateToFlavorScreen() {
+        composeTestRule.onNodeWithStringId(com.example.cupcake.R.string.one_cupcake)
+            .performClick()
+        // first click to select one cupcake
+
+
+        composeTestRule.onNodeWithStringId(com.example.cupcake.R.string.chocolate)
+            .performClick()
+        // second click to select cupcake flavour - clicking next from this point will navigate to the next screen
+    }
+
+    private fun navigateToPickupScreen() {
+        navigateToFlavorScreen()
+        composeTestRule.onNodeWithStringId(com.example.cupcake.R.string.next)
+            .performClick()
+    }
+
+    private fun navigateToSummaryScreen() {
+        navigateToPickupScreen()
+        composeTestRule.onNodeWithText(getFormattedDate())
+            .performClick()
+    }
+
+//test naming convention
+// thingUnderTest_TriggerOfTest_ResultOfTest
+
+    @Test
+    fun cupcakeNavHost_verifyStartDestination() {
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
+
+    @Test
+    fun cupcakeNavHost_verifyBackNavigationNotShownOnStartOrderScreen() {
+        val backText = composeTestRule.activity.getString(com.example.cupcake.R.string.back_button)
+        composeTestRule.onNodeWithContentDescription(backText).assertDoesNotExist()
+    }
+
+    @Test
+    fun cupcakeNavHost_clickOneCupcake_navigatesToSelectFlavorScreen() {
+        composeTestRule.onNodeWithStringId(com.example.cupcake.R.string.one_cupcake)
+            .performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Flavor.name)
+    }
+
+    @Test
+    fun cupcakeNavHost_clickUpButtonFromFlavorScreen_navigatesToStartScreen() {
+        navigateToFlavorScreen()
+        composeTestRule.onNodeWithStringId(com.example.cupcake.R.string.back_button)
+            .performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
+
+    @Test
+    fun cupcakeNavHost_clickCancelButtonFromFlavorScreen_navigatesToStartScreen() {
+        navigateToFlavorScreen()
+        composeTestRule.onNodeWithStringId(com.example.cupcake.R.string.cancel)
+            .performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
+
+    @Test
+    fun cupcakeNavHost_navigateToPickupScreen() {
+        navigateToPickupScreen()
+        navController.assertCurrentRouteName(CupcakeScreen.Pickup.name)
+    }
+
+//    @Test
+//    fun cupcakeNavHost_clickUpButtonFromPickupScreen_navigatesToFlavorScreen() {
+//        navigateToPickupScreen()
+//        composeTestRule.onNodeWithStringId(com.example.cupcake.R.string.back_button)
+//        navController.assertCurrentRouteName(CupcakeScreen.)
+//    }
+
+
+
+
+
+
+}
+
---
+++ Cupcake/app/src/androidTest/java/com/example/cupcake/test/ScreenAssertions.kt
@@ -0,0 +1,10 @@
+package com.example.cupcake.test
+
+import androidx.navigation.NavController
+import junit.framework.Assert.assertEquals
+
+fun NavController.assertCurrentRouteName(
+    expectedRouteName: String
+) {
+    assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
